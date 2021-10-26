f0d3742cc4d2c7a09d3fe7dc7aab4d45caf69b3d
orhungorkem <gorkem.orhun98@gmail.com>
2021-10-26 15:39:01 +0200

    login menu

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -8,6 +8,27 @@ public class Client {
 
     public void printMenu(){
         //the menu with available options
+        System.out.println("-------------Menu--------------");
+        System.out.println("Press 1 to print");
+        System.out.println("Press 2 to see queue");
+        System.out.println("Press 3 to put a job to top of the queue");
+        System.out.println("Press 4 to restart server");
+        System.out.println("Press 5 to stop server");
+        System.out.println("Press 6 to see printer status");
+        System.out.println("Press 0 to Authenticate");
+        //may add read config set config
+
+    }
+
+
+    public void loginMenu(Scanner in, Printer printServer) throws RemoteException {
+        System.out.print("Username: ");
+        String username = in.next();
+        System.out.print("Password: ");
+        String password = in.next();
+
+        System.out.println(printServer.start(username,password));
+
     }
 
     public static void main(String [] args) throws MalformedURLException, NotBoundException, RemoteException {
@@ -16,7 +37,13 @@ public class Client {
 
         Printer printServer = (Printer) Naming.lookup("rmi://localhost:3099/printer");
 
+
+
+
         boolean on = true;
+
+        System.out.print("Enter a string: ");
+        String str= in.nextLine();
 
         while(on){
             break;
