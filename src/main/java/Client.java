b0b1852f07e98c525e179949ecc069ca7cd8cf8d
Marialena Kostopoulou <marialena.kostopoulou@outlook.com>
2021-11-08 15:55:51 +0100

    Add login/out steps

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -8,6 +8,7 @@ public class Client {
     public static void printMenu(){
         //the menu with available options
         System.out.println("-------------Menu--------------");
+        System.out.println("Press 0 to start server");
         System.out.println("Press 1 to print");
         System.out.println("Press 2 to see queue");
         System.out.println("Press 3 to put a job to top of the queue");
@@ -16,8 +17,9 @@ public class Client {
         System.out.println("Press 6 to see printer status");
         System.out.println("Press 7 to see a user configuration parameter");
         System.out.println("Press 8 to edit a user configuration parameter");
-        System.out.println("Press 9 to turn off");
-        System.out.println("Press 0 to Authenticate");
+        System.out.println("Press 9 to login");
+        System.out.println("Press 10 to logout");
+        System.out.println("Press 11 to turn off application");
     }
 
     public static void loginMenu(Scanner in, Printer printServer) throws RemoteException {
@@ -25,8 +27,7 @@ public class Client {
         String username = in.next();
         System.out.print("Password: ");
         String password = in.next();
-        System.out.println(printServer.start(username,password));
-
+        System.out.println(printServer.login(username,password));
     }
 
     public static void main(String [] args) throws MalformedURLException, NotBoundException, RemoteException {
@@ -36,67 +37,124 @@ public class Client {
             printMenu();
             String choice = in.next();
             if(choice.equals("0")) {
-                loginMenu(in, printServer);
+                String response = printServer.start();
+                System.out.println(response);
             }
             else if(choice.equals("1")){
-                System.out.print("Printer name: ");
-                String printer = in.next();
-                System.out.println("");
-                System.out.print("File name: ");
-                String filename = in.next();
-                System.out.println("");
-                String response = printServer.print(filename,printer);
+                // Check server is up
+                String response = "";
+                if (printServer.isPrintServerStopped()){
+                    System.out.println("");
+                    response = "Printer server is not running.";
+                } else {
+                    System.out.print("Printer name: ");
+                    String printer = in.next();
+                    System.out.println("");
+                    System.out.print("File name: ");
+                    String filename = in.next();
+                    System.out.println("");
+                    response = printServer.print(filename, printer);
+                }
                 System.out.println(response);
             }
             else if(choice.equals("2")){
-                System.out.print("Printer name: ");
-                String printer = in.next();
-                System.out.println("");
-                String response = printServer.queue(printer);
+                String response = "";
+                if (printServer.isPrintServerStopped()){
+                    System.out.println("");
+                    response = "Printer server is not running.";
+                } else {
+                    System.out.print("Printer name: ");
+                    String printer = in.next();
+                    System.out.println("");
+                    response = printServer.queue(printer);
+                }
                 System.out.println(response);
             }
             else if(choice.equals("3")){
-                System.out.print("Printer name: ");
-                String printer = in.next();
-                System.out.println("");
-                System.out.print("Job id: ");
-                int job = in.nextInt();
-                System.out.println("");
-                String response = printServer.topQueue(printer,job);
+                String response = "";
+                if (printServer.isPrintServerStopped()){
+                    System.out.println("");
+                    response = "Printer server is not running.";
+                } else {
+                    System.out.print("Printer name: ");
+                    String printer = in.next();
+                    System.out.println("");
+                    System.out.print("Job id: ");
+                    int job = in.nextInt();
+                    System.out.println("");
+                    response = printServer.topQueue(printer,job);
+                }
                 System.out.println(response);
             }
             else if(choice.equals("4")){
-                String response = printServer.restart();
+                String response = "";
+                if (printServer.isPrintServerStopped()){
+                    System.out.println("");
+                    response = "Printer server is not running.";
+                } else {
+                 response = printServer.restart();
+                }
                 System.out.println(response);
             }
             else if(choice.equals("5")){
-                String response = printServer.stop();
+                String response = "";
+                if (printServer.isPrintServerStopped()){
+                    System.out.println("");
+                    response = "Printer server is not running.";
+                } else {
+                    response = printServer.stop();
+                }
                 System.out.println(response);
             }
             else if(choice.equals("6")){
-                System.out.print("Printer name: ");
-                String printer = in.next();
-                System.out.println("");
-                String response = printServer.status(printer);
+                String response = "";
+                if (printServer.isPrintServerStopped()){
+                    System.out.println("");
+                    response = "Printer server is not running.";
+                } else {
+                    System.out.print("Printer name: ");
+                    String printer = in.next();
+                    System.out.println("");
+                    response = printServer.status(printer);
+                }
                 System.out.println(response);
             }
             else if(choice.equals("7")){
-                System.out.print("Type the parameter name: ");
-                String parameter = in.next();
-                System.out.println("");
-                String response = printServer.readConfig(parameter);
+                String response = "";
+                if (printServer.isPrintServerStopped()){
+                    System.out.println("");
+                    response = "Printer server is not running.";
+                } else {
+                    System.out.print("Type the parameter name: ");
+                    String parameter = in.next();
+                    System.out.println("");
+                    response = printServer.readConfig(parameter);
+                }
                 System.out.println(response);
             }
             else if(choice.equals("8")){
-                System.out.print("Type the parameter name: ");
-                String parameter = in.next();
-                System.out.print("Type the parameter value: ");
-                String value = in.next();
-                System.out.println("");
-                String response = printServer.setConfig(parameter, value);
+                String response = "";
+                if (printServer.isPrintServerStopped()){
+                    System.out.println("");
+                    response = "Printer server is not running.";
+                } else {
+                    System.out.print("Type the parameter name: ");
+                    String parameter = in.next();
+                    System.out.print("Type the parameter value: ");
+                    String value = in.next();
+                    System.out.println("");
+                    response = printServer.setConfig(parameter, value);
+                }
                 System.out.println(response);
             }
-            else  if (choice.equals("9")){
+            else if(choice.equals("9")) {
+                loginMenu(in, printServer);
+            }
+            else if(choice.equals("10")){
+                String response = printServer.logout();
+                System.out.println(response);
+            }
+            else  if (choice.equals("11")){
                 System.out.println("Turning off...");
                 break;
             } else {
